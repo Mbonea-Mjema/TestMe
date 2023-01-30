b70b2843a131f4eb1a0e7ab86f58788ad32e1b6c
Mbonea-Mjema <mjema86@gmail.com>
2023-01-30 07:10:14 +0000

    added all the pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["image.tmdb.org", "telegra.ph", "images.jedavi2095.workers.dev"],
